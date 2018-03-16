e09e2717f647d4fa48eb13fc2bd104d9fd89bc7b
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-03-16 18:53:58 +0100

    tries to fix API docs

--- conf.py
+++ conf.py
@@ -1,5 +1,15 @@
-extensions = ['autoapi.extension']
+# -*- coding: utf-8 -*-
+import sys
+import os
 
-# Document Python Code
+needs_sphinx = '1.3'
+extensions = ['autoapi.extension']
 autoapi_type = 'python'
 autoapi_dirs = ['api']
+source_suffix = '.md'
+master_doc = 'index'
+project = u'Datadots API for Python'
+copyright = u'2018, Michael Hausenblas'
+author = u'Michael Hausenblas'
+version = u'0.1'
+release = u'0.1'
