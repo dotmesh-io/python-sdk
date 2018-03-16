4fd8811e507c671019eb3238fefaa7545e78adc0
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-03-16 19:22:53 +0100

    tries one very last API doc fix

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@ def run_apidoc(_):
 	import sys
 	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 	cur_dir = os.path.abspath(os.path.dirname(__file__))
-	module = os.path.join(cur_dir, "api")
+	module = os.path.join(cur_dir, "..", "api")
 	main(['-e', '-o', cur_dir, module, '--force'])
 
 def setup(app):
