9192a02846f3b8f7c788122258030d24ab64efc1
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-03-16 08:43:25 +0100

    advances API

---
+++ api/client.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+This module implements the Dotmesh client.
+"""
+
+
+def DotmeshClient(clusterURL="localhost", username="admin", api_key):
+    print("not yet implemented")
+
