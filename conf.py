30f4ca2ddd568dd679ff1879690fa7b3978f1ede
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-03-16 19:17:06 +0100

    tries yet another way to fix API docs

--- conf.py
+++ conf.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
-extensions = ['autoapi.extension']
+# extensions = ['autoapi.extension']
+
+def run_apidoc(_):
+    modules = ['api']
+    for module in modules:
+        cur_dir = os.path.abspath(os.path.dirname(__file__))
+        output_path = os.path.join(cur_dir, module, 'doc')
+        cmd_path = 'sphinx-apidoc'
+        if hasattr(sys, 'real_prefix'):
+            cmd_path = os.path.abspath(os.path.join( sys.prefix, 'bin', 'sphinx-apidoc'))
+        subprocess.check_call([cmd_path, '-e', '-o', output_path, module, '--force'])
+
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
