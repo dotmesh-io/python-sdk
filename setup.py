d76c0a06edc3880ec08f6ccdb8b10aaed1f57584
Luke Marsden <me@lukemarsden.net>
2018-09-04 12:13:12 +0100

    #3: Bump datadots-api version to 0.1.6 so that consumers can depend on the new Dot.fromNamespaceAndName and Dot.fromDotNameWithOptionalNamespace APIs

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from codecs import open
 
 setup(
     name='datadots-api',
-    version='0.1.5',
+    version='0.1.6',
     description='Datadots API',  
     long_description=open('README.md', 'r').read(),
     long_description_content_type="text/markdown",
